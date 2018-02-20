e833a99e18011f3a5f94b301e5e58913d466f0f4
Jeff Warnica <jeff@coherentnetworksolutions.com>
2018-02-20 11:04:35 -0400

    stuff

--- factories/Cflab/core.rb
+++ factories/Cflab/core.rb
@@ -8,7 +8,6 @@ end
 
 
 shared_examples_for "Core StdLib" do
-  puts "IM A SHARED EXAMPLE of [#{described_class}]"
   # dump_root = instance_double("dump_root")
   before { allow_any_instance_of(described_class).to receive(:dump_root) {
     # Stub implementation here
